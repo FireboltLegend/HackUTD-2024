81395ecb11a7b1db065b877806bbc12828b02d48
FireboltLegend <abbas.khawaja@gmail.com>
2024-11-17 00:20:17 -0600

    Fixing URP

--- Assets/Plugins/Coffey Utils/Editor/Attributes/HighlightAttributeDrawer.cs
+++
@@ -1,9 +0,0 @@
-#if UNITY_EDITOR
-using UnityEditor;
-
-[CustomPropertyDrawer(typeof(HighlightAttribute))]
-public class HighlightAttributeDrawer : HighlightableAttributeDrawer
-{
-    protected override bool ShouldHighlight(SerializedProperty property) => true;
-}
-#endif
--- Assets/Plugins/Coffey Utils/Utilities/Documentation.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class Documentation : MonoBehaviour
-{
-    [SerializeField] private TextAsset _text = null;
-    [SerializeField] private Color _textColor = Color.white;
-
-    public string Text => _text != null ? _text.text : "";
-}
