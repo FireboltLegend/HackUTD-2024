26b483611b1038936670dbfa2ac16c33b5070cc9
vivi <43019257+vivi-j@users.noreply.github.com>
2024-11-17 10:38:55 -0600

    Added Controlled Haptics

--- Assets/HapticInteraction.cs
+++ Assets/HapticInteraction.cs
@@ -6,26 +6,23 @@ using TMPro;
 
 public class HapticInteraction : MonoBehaviour
 {
-    public TextMeshProUGUI textMeshProUGUI;
-    public string hands;
-
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Interactable"))
-            StartHaptics(other.gameObject, textMeshProUGUI);
+            StartHaptics();
     }
     private void OnTriggerExit(Collider other)
     {
         if(other.CompareTag("Interactable"))
             StopHaptics();
     }
-    private void StartHaptics(GameObject gameObject, TextMeshProUGUI textMeshProUGUI)
+    private void StartHaptics()
     {
-        BhapticsLibrary.PlayParam(hands, intensity : 1, duration : 1);
+        BhapticsLibrary.PlayParam("hands", intensity : 1, duration : 1);
         Debug.Log("No haptic device connected");
     }
     void StopHaptics()
     {
-        
+        BhapticsLibrary.StopAll();
     }
 }
